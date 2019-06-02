425c376a6c2e773049ff25dd4125dd183936ba26
thiagohenriquesantos <thiagosantosengcivil@gmail.com>
2019-06-02 13:26:05 -0300

    add view creating events

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -4,6 +4,9 @@ class EventController {
   index({ view }) {
     return view.render('event.index')
   }
+  create({ view }) {
+    return view.render('event.create')
+  }
 }
 
 module.exports = EventController
